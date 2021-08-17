9799a29d250701fc79c0bcfe139477143b803a07
Martin Dammrath <martin.dammrath@gmail.com>
2021-08-17 23:13:04 +0200

    add some content

--- src/App.js
+++ src/App.js
@@ -8,19 +8,13 @@ class App extends Component {
     const currentDate = new Date();
     const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Countdown</h2>
-
-          <a href="https://github.com/kristinbaumann/react-countdown" target="_blank">
-            <img src={github} alt="github" className="githubIcon" />
-            <span>View on Github</span>
-          </a>
-        </div>
-
-        <h3 className="title">Christmas Eve is coming soon (Midnight of 23rd to 24th Dec, UTC time):</h3>
-        <Countdown date={`${year}-12-24T00:00:00`} />
+  
+      <div className="App" style={{background: "none", backgroundImage: "linear-gradient(-45deg,rgba(113,79,155,.5),#714f9b)", color: "white", display: "table", position: "relative", height: "100vh", width: "100vw", textAlign: "center", fontFamily:  "'Montserrat', sans-serif"}}>
+      <span style={{verticalAlign: "middle", display: "table-cell", marginBottom: "30px"}}>
+        <h1>&bdquo;Das Leben ist viel zu kurz, um schlechten Wein zu trinken.&ldquo; <br/> <span style={{fontSize: "20px", color: "lightgrey"}}>– Johann Wolfgang von Goethe</span></h1>
+        <hr style={{marginLeft: "10%", marginRight: "10%", border: "0", borderBottom: "1px solid white"}}/>
+        <span style={{fontSize: "3em"}}><Countdown date={`${year}-09-15T00:00:00`} /></span>
+        </span>
       </div>
     );
   }
